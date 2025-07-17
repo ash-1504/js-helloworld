0e6d9994c98038a540f7aff83eb5760432db11e8
ash-1504 <aastha.shrivasta.1504@gmail.com>
2025-07-17 12:45:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //trat all js code as newer version of js
+ 
+// alert(3+3) //will give error becoz we are running it in nodejs not browser
+let name = "aastha"
+let age = 21
+let isloggedin = false 
+
+//number = 2 to the power of 53
+//bigint  used only for high numbers usualy not used (stock market)
+//boolean = true/false
+//null = standalone value
+//undefined 
+//symbol to find uniquness 
+//object    
+//these the the data types in js
+
+console.log(typeof null); // this will give out put as object
+console.log(typeof undefined); //this will give out put as undefined
+console.log(typeof "aastha"); //this will give out put as string
+console.log(typeof age); //this will give out put as number
+
