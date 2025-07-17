d289276edcfe910c02859ac50ee24ba537757bf8
ash-1504 <aastha.shrivasta.1504@gmail.com>
2025-07-17 13:14:14 +0000

    conversionoperation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score)); /* we can also write score in () as methods
+ methods are used to manipulate objects they are written in the same way as funtions 
+ but defined within the objects*/
+
+let valueInNumber = Number(score) /*here we have converted the datatype of score from string to number
+in valueInNumber*/
+console.log(typeof valueInNumber);
+
+/* if the value of scor = "33abc" then its not a number how will it covert that into a number therefore
+it will give NAN ie not a number we have to chack carefully while converting */
+
+console.log(valueInNumber);
+
+//if the value is string or undefined then output will be NAN
+//if the value is null it will give 0
+//if the value is in boolean form like if true then it will give 1 else false it will give 0 
