ab43d1ec9c38042624a3a4781a87a648829f8b58
ash-1504 <aastha.shrivasta.1504@gmail.com>
2025-07-22 16:03:00 +0000

    dates

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -6,6 +6,28 @@ console.log(myDate.toDateString());// this will give day and date
 console.log(myDate.toLocaleString());//this will give date and time
 console.log(typeof myDate);//this will give output as object as we have defined it as object in the starting
 
+let myCreatedDate = new Date(2004, 3 , 15)
+console.log(myCreatedDate.toLocaleDateString());// locale date string will give in the formal of MM/DD/YYYY
+//BUT the month numbering starts from 0 so if i have give 4 there will be may 
 
-let myCreatedDate = new Date(2004, 4 , 15)
-console.log(myCreatedDate.toLocaleDateString());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime()); // this use to get the time stamp used to compare values 
+//used in game like racing where time in sec matters
+
+console.log(Math.floor(Date.now()/1000))
+// /1000 used so that it can get cinverted from milisec to sec
+//math.floor used to get the floor value
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());//this will only give the day
+console.log(newDate.getMonth());//this will only give the month
+
+`$({newDate.getDay()}and time`
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+
